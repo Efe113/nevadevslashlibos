acc2ce9b3d2336265739f8dbaf983797dd62e8e8
EfeTR <efedemirkol61@gmail.com>
2025-07-07 15:10:41 +0300

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -3,7 +3,7 @@ const { Events } = require('discord.js');
 module.exports = {
     name: Events.InteractionCreate,
     async execute(interaction) {
-        // Etkileşim bir slash komutu değilse veya bottan geliyorsa işlem yapma
+        
         if (!interaction.isChatInputCommand()) return;
 
         const command = interaction.client.commands.get(interaction.commandName);
@@ -14,11 +14,11 @@ module.exports = {
         }
 
         try {
-            // Komutu çalıştır
+            
             await command.execute(interaction);
         } catch (error) {
             console.error(error);
-            // Kullanıcıya hata mesajı gönder
+            
             if (interaction.replied || interaction.deferred) {
                 await interaction.followUp({ content: 'Bu komutu çalıştırırken bir hata oluştu!', ephemeral: true });
             } else {
