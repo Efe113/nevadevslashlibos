1b12c35ea7dcb693639dd1d676cda799a5009bcf
EfeTR <efedemirkol61@gmail.com>
2025-07-07 15:10:21 +0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -5,7 +5,6 @@ module.exports = {
         .setName('ping')
         .setDescription('Botun gecikme süresini gösterir.'),
     async execute(interaction) {
-        // 'interaction.client.ws.ping' botun Discord API'sine olan gecikmesini ms cinsinden verir.
         await interaction.reply(`Pong! 🏓 Bot Gecikmesi: ${interaction.client.ws.ping}ms`);
     },
 };
