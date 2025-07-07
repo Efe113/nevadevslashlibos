146a6e2086d1d50f6d0c6c3e6b92df6b39a4830f
EfeTR <efedemirkol61@gmail.com>
2025-07-07 15:11:46 +0300

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,10 +1,9 @@
 const { REST, Routes } = require('discord.js');
 const fs = require('node:fs');
 const path = require('node:path');
-require('dotenv').config(); // .env dosyasındaki değişkenleri yükler
+require('dotenv').config(); 
 
 const commands = [];
-// commands klasöründeki tüm komut dosyalarını oku
 const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
@@ -13,15 +12,15 @@ for (const file of commandFiles) {
     commands.push(command.data.toJSON());
 }
 
-// REST modülünün bir örneğini oluştur ve token'ı ayarla
+
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
-// Komutları dağıt!
+
 (async () => {
     try {
         console.log(`${commands.length} adet (/) komutu yenileniyor.`);
 
-        // Komutları belirtilen sunucuya (guild) göndermek için 'put' metodu
+        
         const data = await rest.put(
             Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
             { body: commands },
@@ -29,7 +28,7 @@ const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
         console.log(`${data.length} adet (/) komutu başarıyla yüklendi.`);
     } catch (error) {
-        // Hataları yakala ve konsola yazdır
+        
         console.error(error);
     }
 })();
